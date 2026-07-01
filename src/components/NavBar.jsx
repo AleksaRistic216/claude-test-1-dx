23a79b9aead2cf47cf3e792f89c4c71884ab990c
CORP\aleksa.ristic <aleksa.ristic@devexpress.com>
2026-07-01 21:12:31 +0400

    Add Issues link to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,7 @@ import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
 import SettingsIcon from '@mui/icons-material/Settings'
 import CodeIcon from '@mui/icons-material/Code'
+import ListAltIcon from '@mui/icons-material/ListAlt'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
@@ -21,6 +22,15 @@ export default function NavBar() {
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           GitHub Viewer
         </Typography>
+        <Link href="/issues" passHref legacyBehavior>
+          <IconButton
+            color="inherit"
+            aria-label="issues"
+            sx={{ opacity: router.pathname === '/issues' ? 1 : 0.75 }}
+          >
+            <ListAltIcon />
+          </IconButton>
+        </Link>
         <Link href="/settings" passHref legacyBehavior>
           <IconButton
             color="inherit"
